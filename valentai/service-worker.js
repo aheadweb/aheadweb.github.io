7f340b48c4a77b4bbf0b3e8b411567766a87c89a
aheadweb <xalinanton@mail.ru>
2020-01-03 20:27:04 +0700

    * api first test

--- valentai/service-worker.js
+++ valentai/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aheadweb/aheadweb.github.io/precache-manifest.880b2c9f506f0668ccf04da84e06fb24.js"
+  "/precache-manifest.2dd7e80b1a6a55359e4b40338d5920ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aheadweb/aheadweb.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
