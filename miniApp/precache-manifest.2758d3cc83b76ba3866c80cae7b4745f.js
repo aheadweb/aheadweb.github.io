0fd5ad6757600ae3398b479707fbc3e60b973c90
aheadweb <xalinanton@mail.ru>
2020-11-07 19:56:19 +0700

    + add miniapp

---
+++ miniApp/precache-manifest.2758d3cc83b76ba3866c80cae7b4745f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f1809ce7e101ef73791c50c89e2653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09771bfd337aaef41170",
+    "url": "/static/css/2.6326594c.chunk.css"
+  },
+  {
+    "revision": "a54208f9f3e11623fdca",
+    "url": "/static/css/main.c869b2a5.chunk.css"
+  },
+  {
+    "revision": "09771bfd337aaef41170",
+    "url": "/static/js/2.083e0e7c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.083e0e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a54208f9f3e11623fdca",
+    "url": "/static/js/main.cdfe93ac.chunk.js"
+  },
+  {
+    "revision": "1f01fa26ed4dfd25fcea",
+    "url": "/static/js/runtime-main.ff33dd48.js"
+  }
+]);
